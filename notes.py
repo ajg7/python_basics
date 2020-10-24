a22e6f779628e4243b8c3a351013b33e5da40240
ajg7 <ajgebara@gmail.com>
2020-10-24 15:46:25 -0500

    started writing python notes

---
+++ notes.py
@@ -0,0 +1,11 @@
+# This is a comment
+# how to "console.log" in python
+print("hello World")
+
+# White Space
+
+school = "Lambda"
+if school == "Lambda":
+    # indentation of four spaces
+    print("school is 'Lambda'.")
+    
